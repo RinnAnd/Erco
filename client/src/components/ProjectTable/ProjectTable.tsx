5fc77d5f617d0b167b37bd12db9ba97a103b58ed
RinnAnd <107440163+RinnAnd@users.noreply.github.com>
2023-08-04 12:31:55 -0500

    fixed some buttons, added search input <Change single project view for project list when finding projects

--- client/src/components/ProjectTable/ProjectTable.tsx
+++ client/src/components/ProjectTable/ProjectTable.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC, FormEvent, useState } from "react";
 import "./ProjectTable.css";
 import { Project, type Projects } from "../../types";
 import SingleProject from "../SingleProject/SingleProject";
@@ -9,13 +9,32 @@ interface ProjectTableProps {
 
 const ProjectTable: FC<ProjectTableProps> = ({ projects }) => {
   const [visibleProject, setVisibleProject] = useState<Project | null>(null);
+  const [input, setInput] = useState("");
+  const [error, setError] = useState(false);
+  
+  
 
   function showProject(project: Project) {
     setVisibleProject({ ...project });
   }
 
+  function findProject(e:FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    let projectToFind = projects?.filter(pr => pr.system_name.toLowerCase().includes(input.toLowerCase()))
+    if (projectToFind && projectToFind[0]) {
+      showProject(projectToFind[0])
+    } else {
+      setError(true)
+    }
+  }
+
   return (
     <section className="table_section">
+      <form onSubmit={(e) => findProject(e)} className="search_form">
+        <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+        <button type="submit">Find Project</button>
+        {error ? <div onClick={() => setError(false)}>Project not found</div> : <></>}
+      </form>
       {visibleProject ? (
         <SingleProject project={visibleProject} setter={setVisibleProject} />
       ) : (
